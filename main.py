c960a5458fc95a1cc5e8501778368fbc82e781c0
Arely Rosendes <141471552+arelyyr@users.noreply.github.com>
2023-11-30 20:04:45 -0800

    main.py

--- main.py
+++ main.py
@@ -11,15 +11,19 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input("How many symbols would you like?\n"))
 nr_numbers = int(input("How many numbers would you like?\n"))
 
+# Randomize letters chosen
 for _ in range(nr_letters):
     pw.append(random.choice(letters))
 
+# Randomize symbols chosen
 for _ in range(nr_symbols):
     pw.append(random.choice(symbols))
 
+# Randomize numbers chosen
 for _ in range(nr_numbers):
     pw.append(random.choice(numbers))
 
+# Randomize all letters, numbers, and symbols
 random.shuffle(pw)
 
 password = "".join(pw)
